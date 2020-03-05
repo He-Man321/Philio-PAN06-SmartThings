debeeb9efcb6bd400c7c8fc247388b7089c55469
He-Man321 <43375882+He-Man321@users.noreply.github.com>
2020-03-05 10:03:59 +0000

    V1.1
    Added some handlers for random events that may happen and simplified a few bits.

--- Philio-PAN06.groovy
+++ Philio-PAN06.groovy
@@ -1,4 +1,8 @@
-/* Philio PAN06 Dual Relay by He-Man321*/ 
+/* Philio PAN06 Dual Relay by He-Man321
+Version 1.1 - 2020/03/05
+NOTE: 	This nust create the chid devices as 1 and 2 (e.g. 22-sw1 and 22-sw2). I have seen it create them with other numbers, which won't work - AI - 19/09
+		Also check the Current States section of the device in the IDE, it must show switch1 and switch2 if not, remove and re-add the device - AI - 20/03/05
+*/ 
 metadata {
 definition (name:"Philio PAN06 Dual Relay",namespace:"",author:"He-Man321") {
 capability "Switch"
@@ -16,39 +20,34 @@ command "off1"
 command "on2"
 command "off2"
 }
-
 tiles(scale: 2){
     multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
 			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
-				attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
-                attributeState "turningOn", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
-				attributeState "turningOff", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
+				attributeState "on", label: 'Master', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
+				attributeState "off", label: 'Master', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
+                attributeState "turningOn", label: '${name}', action: "switch.off", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState:"turningOff"
+				attributeState "turningOff", label: '${name}', action: "switch.on", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState:"turningOn"
 			}
 	}
 	standardTile("switch1", "device.switch1",canChangeIcon: false, width: 2, height: 2) {
 		state "on", label: 'S1', action: "off1", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
 		state "off", label: 'S1', action: "on1", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
-        state "turningOn", label: 'S1', action: "off1", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
-		state "turningOff", label: 'S1', action: "on1", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
-        
+        state "turningOn", label: 'S1', action: "off1", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState:"turningOff"
+		state "turningOff", label: 'S1', action: "on1", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState:"turningOn"        
     }
 	standardTile("switch2", "device.switch2",canChangeIcon: false, width: 2, height: 2) {
 		state "on", label: 'S2', action: "off2", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
 		state "off", label: 'S2', action: "on2", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
-        state "turningOn", label: 'S2', action: "off2", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState:"turningOff"
-		state "turningOff", label: 'S2', action: "on2", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
-        
+        state "turningOn", label: 'S2', action: "off2", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState:"turningOff"
+		state "turningOff", label: 'S2', action: "on2", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState:"turningOn"        
     }
     standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
 		state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
     }    
-
     main(["switch","switch1", "switch2"])
     details(["switch","switch1","switch2","refresh"])
 }   
-   
-   preferences {
+preferences {
         def paragraph="Device Settings"
         input name:"param2",type:"number",range:"1..3",defaultValue:"1",required:false,
             title: "Change the physical button mode (e.g. Toggle/Momentary switch).\n" +
@@ -58,7 +57,6 @@ tiles(scale: 2){
                    "Default value: 1."
     }
 }
-
 def parse(String description) {
     def result=[]
     def cmd=zwave.parse(description)
@@ -69,6 +67,15 @@ def parse(String description) {
     return result
 }
 
+def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCapabilityReport cmd) 
+{
+    log.debug "MultiChannelCapabilityReport: $cmd"
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelEndPointReport cmd) {
+	log.debug "MultiChannelEndPointReport: $cmd"
+}
+
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd)
 {
     def result=[]
@@ -76,33 +83,36 @@ def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cm
     result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:2,commandClass:37,command:2).format()
     response(delayBetween(result,1000))
 }
-def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {
+def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd)
+{   
+    def result=[]
+    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:1,commandClass:37,command:2).format()
+    result << zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:2,commandClass:37,command:2).format()
+    response(delayBetween(result,1000))    
+}
+def zwaveEvent(physicalgraph.zwave.commands.multichannelv3.MultiChannelCmdEncap cmd) {    
     def curswitch=cmd.sourceEndPoint
-    def map=[name:"switch$curswitch"]
-	if (cmd.parameter==[0]) map.value="off"
-    if (cmd.parameter==[255]) map.value="on"
-    def curstate=map.value
-    try {
+    def state=cmd.parameter==[255]?"on":"off"
+	try {
     	def childDevice=getChildDevices()?.find {it.deviceNetworkId=="$device.deviceNetworkId-sw${curswitch}"}
-		if (childDevice) childDevice.sendEvent(name: "switch", value: curstate)
+		if (childDevice) childDevice.sendEvent(name: "switch", value: state)
     } catch (e) {
         log.error "Couldn't find child device, probably not created during setup: ${e}"
-    }        
-    def events=[createEvent(map)]
-    if (map.value=="on") events+=[createEvent([name:"switch",value:"on"])]
+    }  
+	if (state=="on") sendEvent(name: "switch", value: "on")
     else {
         def allOff=true
         if (curswitch==2 && device.currentState("switch1").value=="on") allOff=false
         if (curswitch==1 && device.currentState("switch2").value=="on") allOff=false        
-        if (allOff) events+=[createEvent([name:"switch",value:"off"])]
-    }
-    events
+        if (allOff) sendEvent(name: "switch", value: "off")
+    }  
 }
-
+def zwaveEvent(physicalgraph.zwave.commands.versionv1.VersionReport cmd) {
+	log.debug "VersionReport: $cmd"	
+}
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
     log.error "Unhandled event=$cmd"
 }
-
 def refresh() {
 	def cmds=[]
     cmds<<zwave.manufacturerSpecificV2.manufacturerSpecificGet().format()
@@ -110,24 +120,20 @@ def refresh() {
     cmds<<zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:2,commandClass:37,command:2).format()
 	delayBetween(cmds,1000)
 }
-
 def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
 	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
 	log.debug "msr: $msr"
     updateDataValue("MSR", msr)
 }
-
 def poll() {
 	def cmds=[]
 	cmds<<zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:1,commandClass:37,command:2).format()
     cmds<<zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:2,commandClass:37,command:2).format()
 	delayBetween(cmds,1000)
 }
-
 def configure() {
-    zwave.configurationV1.configurationSet(parameterNumber:2,configurationValue:[param2.value]).format()
+    zwave.configurationV1.configurationSet(parameterNumber:2,configurationValue:[settings.param2==null?1:settings.param2]).format()
 }
-
 def updated()
 {
     def cmds=configure()
@@ -142,7 +148,6 @@ def updated()
     }    
     response(cmds)
 }
-
 def childRefresh(String dni) {
     refresh()
 }
@@ -155,26 +160,21 @@ def childOff(String dni) {
 private channelNumber(String dni) {
 	dni.split("-sw")[-1] as Integer
 }
-
 def on() {
 	zwave.switchAllV1.switchAllOn().format()
 }
 def off() {
 	zwave.switchAllV1.switchAllOff().format()
 }
-
 def on1() {
 	zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:1,commandClass:37,command:1,parameter:[255]).format()
 }
-
 def off1() {
 	zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:1,destinationEndPoint:1,commandClass:37,command:1,parameter:[0]).format()
 }
-
 def on2() {
 	zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:3,destinationEndPoint:2,commandClass:37,command:1,parameter:[255]).format()
 }
-
 def off2() {
 	zwave.multiChannelV3.multiChannelCmdEncap(sourceEndPoint:3,destinationEndPoint:2,commandClass:37,command:1,parameter:[0]).format()
 }
